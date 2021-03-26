dcd9aa0c91cd26f7e55f9e2b6c9530d33d8837ed
PhilomathJ <PhilomathJ@gmail.com>
2021-03-26 12:00:34 -0400

    Added objectifyRows
    - added transpose() function
    - added test range for rows dataset
    - added new menu option
    - moved completion toast outside of objectify function

--- HelperFunctions.ts
+++ HelperFunctions.ts
@@ -10,16 +10,26 @@ function log<T>(msg: T): void {
 		switch (typeof msg) {
 			case 'string':
 				console.log(msg);
+				break;
 			case 'object':
 				console.log(Object(msg));
+				break;
 			case 'number':
 			case 'bigint':
 			case 'symbol':
 				console.log(String(msg));
+				break;
 			case 'boolean':
 				console.log(new Boolean(msg));
+				break;
 			default:
 				return;
 		}
 	}
+}
+
+function transpose(a) {
+	// return a[0].map(function (_, c) { return a.map(function (r) { return r[c]; }); });
+	// or in more modern dialect
+	return a[0].map((_, c) => a.map((r) => r[c]));
 }
--- code.ts
+++ code.ts
@@ -1,7 +1,14 @@
 const DEBUG = true;
 
-const testRange = {
+const testRangeCols = {
 	startRow: 1,
+	startCol: 1,
+	rows: 6,
+	cols: 6,
+};
+
+const testRangeRows = {
+	startRow: 10,
 	startCol: 1,
 	rows: 6,
 	cols: 6,
@@ -11,6 +18,7 @@ const testRange = {
  * ObjectifyColumns - Converts a Google Sheets Range 2-dimensional array
  * 	into an object with column headers as keys and rest of column array as values
  * @param	{}
+ * @returns	{void}
  */
 function objectifyColumns({
 	startRow,
@@ -23,24 +31,72 @@ function objectifyColumns({
 	rows: number;
 	cols: number;
 }): void {
-	const ss = SpreadsheetApp.getActiveSpreadsheet();
-	const sheet = ss.getActiveSheet();
+	const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
 
-	const range = sheet.getRange(1, 1, 6, 6);
+	const range = sheet.getRange(startRow, startCol, rows, cols);
 	const values: any[][] = range.getValues();
 
-	console.log(values);
+	log(values);
 
-	ss.toast('Objectifying complete', 'Complete');
+	const headers: any[] = values.shift(); // Remove the headers into their own array
+	const colValues: any[][] = transpose(values); // Swap rows for columns
+
+	let obj: Object = {};
+
+	headers.forEach((h, index) => {
+		obj[h] = colValues[index];
+	});
+
+	log(obj);
+}
+
+/**
+ * ObjectifyRows - Converts a Google Sheets Range 2-dimensional array
+ * 	into an object with row headers as keys and rest of row array as values
+ * @param	{}
+ * @returns	{void}
+ */
+function objectifyRows({
+	startRow,
+	startCol,
+	rows,
+	cols,
+}: {
+	startRow: number;
+	startCol: number;
+	rows: number;
+	cols: number;
+}): void {
+	const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+
+	const range = sheet.getRange(startRow, startCol, rows, cols);
+	const values: any[][] = range.getValues();
+
+	log(values);
+
+	let obj: Object = {};
+
+	values.forEach((row) => {
+		obj[row.shift()] = row; // 1st col is key, rest of row is value
+	});
+
+	log(obj);
 }
 
 function onOpen(): void {
 	SpreadsheetApp.getUi()
 		.createMenu('Objectify')
-		.addItem('Objectify Column', 'callObjectifyColumns')
+		.addItem('Objectify Columns', 'callObjectifyColumns')
+		.addItem('Objectify Rows', 'callObjectifyRows')
 		.addToUi();
 }
 
 function callObjectifyColumns() {
-	objectifyColumns(testRange);
+	objectifyColumns(testRangeCols);
+	SpreadsheetApp.getActiveSpreadsheet().toast('Objectifying columns complete', 'Complete');
+}
+
+function callObjectifyRows() {
+	objectifyRows(testRangeRows);
+	SpreadsheetApp.getActiveSpreadsheet().toast('Objectifying rows complete', 'Complete');
 }
